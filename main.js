cf95e2ec11972b1be0ab1b15624fd0b359a37674
Kuugang <77537302+Kuugang@users.noreply.github.com>
2023-05-27 18:18:05 +0800

    .

--- main.js
+++ main.js
@@ -1,20 +1,9 @@
-const flashlightEffect = document.querySelector('.flashlight-effect');
-const flashlights = document.querySelectorAll('.flashlight-effect div');
-
-document.addEventListener('mousemove', (event) => {
-  const mouseX = event.clientX;
-  const mouseY = event.clientY;
-
-  const flashlightWidth = flashlights[0].offsetWidth;
-  const flashlightHeight = flashlights[0].offsetHeight;
-
-  const flashlightX = mouseX - flashlightWidth / 2;
-  const flashlightY = mouseY - flashlightHeight / 2;
-
-  flashlightEffect.style.left = flashlightX + 'px';
-  flashlightEffect.style.top = flashlightY + 'px';
-
-  flashlights.forEach((flashlight, index) => {
-    flashlight.style.transform = `translate(${flashlightWidth / 2 - (index + 1) * 5}px, ${flashlightHeight / 2 - (index + 1) * 5}px)`;
-  });
-});
+document.onreadystatechange = function() {
+  if (document.readyState !== "complete") {
+    document.querySelector("body").style.visibility = "hidden";
+    document.querySelector(".loader").style.visibility = "visible";
+  } else {
+    document.querySelector(".loader").style.display = "none";
+    document.querySelector("body").style.visibility = "visible";
+  }
+};
